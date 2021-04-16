d7731fd3c1a9f75787320606b5d6ed3e3e0addb4
Rachel Solov <rsolov23@yahoo.com>
2021-04-16 18:03:39 -0500

    consolelogging input value and data hour

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,7 +1,6 @@
-var currentTime = moment().format(`dddd, MMM Do YYYY`);
-var currentHour = moment().format(`HH`);
-var currentDay = document.querySelector(`#currentDay`);
-var saveBtnArr = document.querySelectorAll(`.saveBtn`);
+var currentTime = moment().format("dddd, MMM Do YYYY");
+var currentHour = moment().format("HH");
+var currentDay = document.querySelector("#currentDay");
 var scheduleContainer = document.getElementById("schedule-container");
 
 var timeArr = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
@@ -11,16 +10,25 @@ for (let i = 0; i <= timeArr.length - 1; i++) {
   var row = document.createElement("div");
   row.classList.add("row");
   row.innerHTML = `<div class="col-2" id="hour">${timeArr[i]}:00</div>
-  <textarea class="col-8" placeholder="Event"></textarea>
+  <textarea class="col-8" placeholder="Event" data-hour=${timeArr[i]}></textarea>
   <button class="col-2 saveBtn">Save</button>`;
 
   //append to schedule container
   scheduleContainer.append(row);
 }
+var saveBtnArr = document.querySelectorAll(".saveBtn");
+//loop through save buttons and add event listener to each
+for (let i = 0; i <= saveBtnArr.length - 1; i++) {
+  saveBtnArr[i].addEventListener("click", function (event) {
+    console.log(event.target.parentNode.children[1].value);
+    console.log(event.target.parentNode.children[1].dataset.hour)
+  });
+}
+
 // current time?
 
 // localStorage for saved schedule items
-console.log(currentHour);
+console.log("current time: " + currentHour);
 currentDay.textContent = currentTime;
 
 function saveFunction() {
