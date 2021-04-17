32e39332df2fe2317d38c22e79ca9651ae179272
Rachel Solov <rsolov23@yahoo.com>
2021-04-17 16:22:54 -0500

    completed readme and fixed date display

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,19 +1,27 @@
-var currentTime = moment().format("dddd, MMM Do YYYY");
+$("#currentDate").append(moment().format("dddd, MMM Do YYYY"));
+console.log(currentDate);
 var currentHour = moment().format("HH");
 var currentDay = document.querySelector("#currentDay");
 var scheduleContainer = document.getElementById("schedule-container");
 
-var timeArr = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
+var timeArr = ["09", "10", "11", "12", "13", "14", "15", "16", "17"];
 
 for (let i = 0; i <= timeArr.length - 1; i++) {
   //generate html row
   var row = document.createElement("div");
   row.classList.add("row");
   row.innerHTML = `<div class="col-2" id="hour">${timeArr[i]}:00</div>
-  <textarea class="col-8" placeholder="Event" data-hour=${timeArr[i]}>${
-    localStorage.getItem(timeArr[i]) ? localStorage.getItem(timeArr[i]) : ''
+  <textarea class="col-8 ${
+    currentHour > timeArr[i]
+      ? "past"
+      : currentHour == timeArr[i]
+      ? "present"
+      : "future"
+  }" placeholder="ToDo" data-hour=${timeArr[i]}>${
+    localStorage.getItem(timeArr[i]) ? localStorage.getItem(timeArr[i]) : ""
   }</textarea>
-  <button class="col-2 saveBtn">Save</button>`;
+  <i class="col-2 saveBtn fas fa-save fa-2x"></i>
+ `;
 
   //append to schedule container
   scheduleContainer.append(row);
@@ -27,7 +35,3 @@ for (let i = 0; i <= saveBtnArr.length - 1; i++) {
     localStorage.setItem(userHour, userInput);
   });
 }
-
-// current time?
-
-// localStorage for saved schedule items
--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# Work Day Scheduler Starter Code
+![A screenshot of the web page](/develop/assets/images/Capture.png)
+
+# Work Day Scheduler
+
+## Table of Contents
+
+- [Description](#description)
+- [Instructions](#instructions)
+- [Github Profile](#github-profile)
+- [Link to Workday Scheduler](#link-workday-scheduler)
+- [Contributors](#contributors)
+
+### Description
+
+This is a Workday Scheduler that will help you plan your day!!
+
+### Instructions
+
+1. Decide the time for the entry you would like to create
+2. Click on the box next to the time you decided which will be gray, red or green
+3. Type in the text
+4. Press the strong cyan save button to the right
+5. Conquer your schedule!!
+
+### Github Profile
+
+https://github.com/rsolov23/
+
+### Link to my Password Generator
+
+https://github.com/rsolov23/Workday-Scheduler
+
+### Contributors
+
+- Rachel Solov
+- Mad props to Google, Stack Overflow, and MDN Web Docs❤️
+- Special shout out to Sean, Donte, Andres, Brendan, Megan, Tori, Mike, Bradley, Ask BCS and all my classmates❤️
